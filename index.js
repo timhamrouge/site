0e13570583fd4f832bf4571fe466a9ea624a7374
Tom Taylor <tom.taylor@ultra-as.com>
2018-08-16 21:49:34 +0100

    Big poop

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: `I like to put aubergines up my bottom
-	
-      but my bottom hurts`
+      message: `I like to put aubergines up my bottom but my bottom hurts`
     }
   });
 
@@ -13,3 +11,26 @@ var app2 = new Vue({
         message: "i prefer marrows"
     }
 });
+
+var app4 = new Vue ({
+    el: "#app-4",
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something' }
+        ]
+    }
+})
+
+var app5 = new Vue ({
+    el: "#app-5",
+    data: {
+        message: 'My butt looks sooo big and tasty'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
