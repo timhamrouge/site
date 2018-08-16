7daf01db2ff8a1e7ba7e87aad306dab257e14649
timhamrouge <tim@Tims-MacBook-Pro.local>
2018-08-16 21:17:23 +0100

    vue farting

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: `I like to put aubergines up my bottom
+	
+      but my bottom hurts`
+    }
+  });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: "i prefer marrows"
+    }
+});
